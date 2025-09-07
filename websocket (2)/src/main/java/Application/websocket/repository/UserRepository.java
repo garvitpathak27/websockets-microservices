743fe3e90e5c77c6f4e81c37349c89d185025d42
garvitpathak27 <garvitpathak2003@gmail.com>
2025-09-07 23:39:34 +0530

    websocket-microservice add

---
+++ websocket (2)/src/main/java/Application/websocket/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package Application.websocket.repository;
+
+import Application.websocket.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    // You can add custom query methods here if needed
+}
